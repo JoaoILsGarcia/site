8ff586a72a028003170bd5721f2733c3b025f32c
João Garcia <joaogarcia.dl@gmail.com>
2026-02-15 13:28:32 -0300

    desafio javaScript

---
+++ Aulas de javaScript/script.js
@@ -0,0 +1,35 @@
+//função de realiza o calculo
+function calcularDesconto() {
+    //lê os valores digitados pelo usuario
+    const precoOriginal = parseFloat(document.getElementById("precoOriginal").value);
+    const porcentagemDesconto = parseFloat(document.getElementById("porcentagemDesconto").value);
+
+    //verifica de algum valor é negtivo ou não é numero válido
+    if (isNaN(precoOriginal) || isNaN(porcentagemDesconto) || precoOriginal < 0 || porcentagemDesconto < 0) 
+    {
+        alert("Por favor, insira valores numéricos não-negativos para preço e desconto.");
+        console.log ("Entrada inválida: valores negativos ou vazio.");
+
+    } 
+    else {
+        
+        //calcula o valor de desconto e o preço final 
+        const desconto = precoOriginal * (porcentagemDesconto / 100);
+        const precoFinal = precoOriginal - desconto;
+
+        // exibe o resultado no console
+        console.log("Preço original: R$" + precoOriginal.toFixed(2));
+        console.log("Valor do desconto: R$" + desconto.toFixed(2));
+        console.log("Preço final: R$" + precoFinal.toFixed(2));
+
+        //mostra o resultado em um alerta
+        alert(
+            "Resultado:\n" + 
+            "Preço Original: R$" + precoOriginal.toFixed(2) + "\n" +
+            "valor do desconto: R$" + desconto.toFixed(2) + "\n" +
+            "Preço final: R$" + precoFinal.toFixed(2)
+      );
+    }
+}
+
+document.getElementById("calcular").addEventListener("click", calcularDesconto);
