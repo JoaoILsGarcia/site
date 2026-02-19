bb6f909a40cdc4f2f973170a0458dc6237fbadd7
João Garcia <joaogarcia.dl@gmail.com>
2026-02-18 22:07:05 -0300

    utilização de array

---
+++ Aulas de javaScript/aulas sobre array/script.js
@@ -0,0 +1,8 @@
+
+const  listaDeFrutas = [ "Banana", "Abacate", "Maçã", "Melancia", "tomate"];
+console.log (listaDeFrutas)
+console.log (listaDeFrutas[3])
+ listaDeFrutas[1]= "laranja";
+ console.log(listaDeFrutas)
+ listaDeFrutas.push("kiwy","uva")
+ console.log(listaDeFrutas)
