47efeb131b91f5be54acc6609530ace99f55007f
João Garcia <joaogarcia.dl@gmail.com>
2026-02-24 00:12:39 -0300

    alterações

--- Aulas de javaScript/Portifólio profissional/script.js
+++ Aulas de javaScript/Portifólio profissional/script.js
@@ -1,1 +1,21 @@
-console.log("Portfólio carregado com sucesso!");
+const btn = document.getElementById("toggle-theme");
+const body = document.body;
+
+// Carregar tema salvo
+const temaSalvo = localStorage.getItem("tema");
+if (temaSalvo === "dark") {
+  body.classList.add("dark");
+  btn.textContent = "☀️ Tema claro";
+}
+
+btn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    btn.textContent = "☀️ Tema claro";
+    localStorage.setItem("tema", "dark");
+  } else {
+    btn.textContent = "🌙 Tema escuro";
+    localStorage.setItem("tema", "light");
+  }
+});
