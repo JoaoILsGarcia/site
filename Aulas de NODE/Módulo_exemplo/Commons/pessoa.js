ea273d9f9190e87c6f138d1a095ea37eca57243c
João Garcia <joaogarcia.dl@gmail.com>
2026-02-23 23:23:34 -0300

    atualização  do Github

--- Aulas de NODE/Módulo_exemplo/Commons/pessoa.js
+++
@@ -1,8 +0,0 @@
-module.exports = function () {
-  return {
-    nome: "joão",
-    profissao: "Sapateiro/Programador",
-    idade: "36",
-    estado: "sc"
-  }
-}
--- Aulas de NODE/Módulo_exemplo/moduloCalculadora/appcalculadora.js
+++
@@ -1,8 +0,0 @@
-const {soma, subtracao, multiplicacao, divisao} = require("./calculadora"); 
-
-
-console.log("soma:", soma(20, 2));
-console.log("Subtração:", subtracao(20, 2));
-console.log("Multiplicação:", multiplicacao(20, 2));
-console.log("Divisão:", divisao(20, 2));
-console.log("Divisão por zero:", divisao(10, 0));
